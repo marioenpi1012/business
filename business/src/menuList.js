a243bd300ac77f3dba40909e85f6585c03d7ffd1
Mario Pineda <mariopineda@Marios-MacBook-Air.local>
2022-04-24 00:35:38 -0500

    some description and prices oupdated, responsive desing finish

--- business/src/menuList.js
+++ business/src/menuList.js
@@ -1,47 +1,53 @@
 export const menuList = [
     {
         name:'Frijol con quezo',
-        price:10,
+        price:2.00,
         id:0,
         category:'pupusas'
     },
     {
         name:'Chicharon con quezo',
-        price:10,
+        price:2.00,
         id:1,
         category:'pupusas'
     },
     {
         name:'Loroco con quezo',
-        price:10,
+        price:2.25,
         id:2,
         category:'pupusas'
     },
     {
         name:'Espinaca con quezo',
-        price:10,
+        price:2.25,
         id:3,
+        category:'pupusas'
+    },
+    {
+        name:'quezo',
+        price:2.25,
+        id:4,
         category:'pupusas'
     },
     {
         name:'Taco Huevo con chorizo',
         description:'tortilla de maiz, tortilla de harina + $0.10',
         price:10,
-        id:4,
+        id:5,
         category:'breakfast'
     },
     {
         name:'Taco Huevo con papa',
         description:'tortilla de maiz, tortilla de harina + $0.10',
         price:10,
-        id:5,
+        id:6,
         category:'breakfast'
     },
     {
         name:'Taco Huevo con jamon',
         description:'tortilla de maiz, tortilla de harina + $0.10',
-        price:10,
-        id:6,
+        price:7,
+        id:7,
         category:'breakfast'
     },
     {
@@ -53,21 +59,21 @@ export const menuList = [
     },
     {
         name:'Pollo guisado',
-        description:'n/a',
+        description:'Arroz, ensalada y totitllas de harina o maiz',
         price:10,
         id:9,
         category:'lunch'
     },
     {
         name:'Bisted Ranchero',
-        description:'n/a',
+        description:'Arroz, ensalada y tortillas de harina o maiz',
         price:10,
         id:10,
         category:'lunch'
     },
     {
         name:'Chile rellenos',
-        description:'quezo o picadillo',
+        description:'Arroz, ensalada y tortillas de harina o maiz. Puede elegir entre quezo o picadillo',
         price:10,
         id:11,
         category:'lunch'
