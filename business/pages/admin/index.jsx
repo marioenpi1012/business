783da7cd2f96c97bee8ad9eca5233d3125500809
Mario Pineda <mariopineda@Marios-MacBook-Air.local>
2022-05-30 10:52:44 -0500

    admin get products url fixed

--- business/pages/admin/index.jsx
+++ business/pages/admin/index.jsx
@@ -80,7 +80,7 @@ export default function index({products}) {
 }
 
 export const getServerSideProps = async () =>{
-    const productRes = await axios.get('https://elpanamericano.vercel.app/')
+    const productRes = await axios.get('https://elpanamericano.vercel.app/api/products')
 
     return{
         props:{
