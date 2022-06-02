a7bdf2b93fe8a32eee5caf6b4cee516af21eab56
Mario Pineda <mariopineda@Marios-MacBook-Air.local>
2022-06-02 12:34:39 -0500

    home desc style updated

--- business/.next/static/chunks/webpack.js
+++ business/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d0e55ba0eaf6ab30"; }
+/******/ 		__webpack_require__.h = function() { return "92ff3ddfa8568101"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
